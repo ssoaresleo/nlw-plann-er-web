fc8657651c9faebdda759fb2f884b30cd5b60b81
Leonardo Henrique <leonardohenriquesoarasalvez784@gmail.com>
2024-07-16 16:04:23 -0300

    styles: add style from modal-root

--- src/components/modal/modal-content.tsx
+++ src/components/modal/modal-content.tsx
@@ -6,7 +6,7 @@ interface ModalContent {
 
 export function ModalContent({ children }: ModalContent) {
   return (
-    <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+    <div className="space-y-5">
       {children}
     </div>
   );
--- src/components/modal/modal-root.tsx
+++ src/components/modal/modal-root.tsx
@@ -10,7 +10,7 @@ export function ModalRoot({ children, isOpen }: ModalRootProps) {
     <Fragment>
       {isOpen && (
         <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
-          {children}
+          <div className="bg-zinc-900 w-[640px] rounded-xl py-5 px-6 shadow-shape">{children}</div>
         </div>
       )}
     </Fragment>
